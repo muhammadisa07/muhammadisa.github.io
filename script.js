790504af75da206e5f3708f4a4cd1184b6d46f08
muhammadisa07 <muhammadisa0704@gmail.com>
2021-12-10 03:13:35 +0700

    menambahkan file html,css,js

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuToogle = document.querySelector('.menu-toogle input');
+const nav = document.querySelector('nav ul');
+menuToogle.addEventListener('click', function(){
+nav.classList.toggle('slide');
+});
